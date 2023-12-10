40329f43c9c8d28bb80bd45b7b6dddea69b1d784
Luke Boyle <lboyle@vlocity.com>
2023-12-10 16:24:55 -0500

    set up day 1

---
+++ Day 1/solution.js
@@ -0,0 +1,12 @@
+/*
+  * Project Owner: Luke Boyle
+  * Project:       Advent of Code '23
+  * File Details ----------------------------------------------------------
+    * Created:     10 Dec 2023
+    * Solution:
+  * Work log --------------------------------------------------------------
+    = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = =
+    Date            | Description
+    = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = =
+    10 Dec 2023     | Initial creation
+*/
